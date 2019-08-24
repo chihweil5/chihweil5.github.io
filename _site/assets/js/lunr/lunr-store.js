450139e49d7903eb035b8708db0f6ad4a0860155
ChihWei <cwl.chihweil@gmail.com>
2019-08-23 22:46:09 -0400

    clean up

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "First Post",
+        "excerpt":"First   今天跟學長還有點顛一起去紐約吃吃喝喝好開心。明天還要一起去買吉他，讚讚！  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/first-post/",
+        "teaser":null}]
