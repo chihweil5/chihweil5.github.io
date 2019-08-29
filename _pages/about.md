4520c57b9388eeb97845f62ab5091bef701fc775
Chih-Wei Lin <cwl.chihweil@gmail.com>
2019-08-28 23:51:13 -0400

    Where it begins - 1

--- _pages/about.md
+++ _pages/about.md
@@ -2,11 +2,8 @@
 layout: single
 permalink: /about/
 title: "About"
-excerpt: "Dooooopy is lazy."
+excerpt: "Doopy"
 author_profile: true
 last_modified_at: 2019-08-24T15:15:09-04:00
 toc: true
 ---
-
-# About me
-大家好我是肚皮
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "First Post",
-        "excerpt":"First   今天跟學長還有點顛一起去紐約吃吃喝喝好開心。明天還要一起去買吉他，讚讚！  ","categories": [],
+        "title": "Where It Begins",
+        "excerpt":"到美國不知不覺也三年了，決定紀錄一下生活，還有分享美國景點給大家，所以就從陪伴我兩年我最愛的Boulder開始吧！   科羅拉多州小科普   在申請學校之前我也沒聽過Colorado這個地方，中西部聽起來很荒涼讓人想到電影西部牛仔的場景…但是科州完全顛覆了我的想像，這裡絕對是世外桃源！   Colorado位於美國中西部，洛磯山脈貫穿西邊，是全美平均海拔最高的一州(2070m)，所以很多運動員會以這邊當作練習的地方，最大城市是Denver，有著名的Rocky Mountain National Park，球隊有NFL Broncos/ MLB Rockies/ NBA Nuggets，科州人夏天不外乎就是camping, hiking, bouldering, biking…冬天就是滑雪拉！oh還有科州是第一個大麻合法化的州。    Boulder   Boulder是科州的第三大城，座落在洛磯山脈山腳下，海拔有1655m，所以剛到的時候爬個三樓就好喘。我很喜歡Boulder的天氣，非常乾燥，極少下雨，食物都不會發霉哈哈，號稱一年365天有350天是晴天，夏天雖然熱但都不太會流汗XD。我的學校 University of Colorado Boulder 就在這個可愛的小城市裡，Boulder不算是一個大學城，還有很多家庭住在這裡，居住環境治安都很好，很適合退休的養老生活。波德的Downtown叫Pearl Street，餐廳和商店聚集的地方，常常舉辦很多活動(eg.萬聖節遊行)   Ref: 維基百科  ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/first-post/",
+        "url": "http://localhost:4000/where-it-begins/",
         "teaser":null}]
